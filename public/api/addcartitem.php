7be6070b8c8b199e068a169a99abbeb7cd2fdaff
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-04-05 15:22:34 -0700

    Added initial cart add

--- public/api/addcartitem.php
+++ public/api/addcartitem.php
@@ -30,10 +30,46 @@
     if (empty($cart_id)) {
         $cart_create_query = "INSERT INTO `carts`
             SET `item_count` = $product_quantity,
-            `total_price` = $product_total,
-            `created` = NOW(),
-            `users_id` = $users_id,
-            `changed` = NOW()";
-        print($cart_create_query);
+                `total_price` = $product_total,
+                `created` = NOW(),
+                `users_id` = $users_id,
+                `changed` = NOW()
+        ";
+        
+        $cart_result = mysqli_query($conn, $cart_create_query);
+
+        if (!$cart_result) {
+            throw new Exception(mysqli_error($conn));
+        }
+
+        if (mysqli_affected_rows($conn) === 0) {
+            throw new Exception('Data was not added to cart table');
+        }
+
+        $cart_id = mysqli_insert_id($conn);
     }
+
+    $cart_item_query = "INSERT INTO `cart_items`
+        SET `products_id` = $product_id,
+            `quantity` = $product_quantity,
+            `carts_id` = $cart_id
+    ";
+
+    $cart_item_result = mysqli_query($conn, $cart_item_query);
+
+    if (!$cart_item_result) {
+        throw new Exception(mysqli_error($conn));
+    }
+
+    if (mysqli_affected_rows($conn) === 0) {
+        throw new Exception('Failed to insert into cart items');
+    }
+
+    $output = [
+        'success' => true,
+        'cartCount' => $product_quantity,
+        'cartTotal' => $product_total
+    ];
+
+    print(json_encode($output));
 ?>
